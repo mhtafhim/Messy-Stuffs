d631ce2474c7cdf4ad88371d08ef55adb37702db
Mahmudul Haque Tafhim <54865949+mhtafhim@users.noreply.github.com>
2022-12-11 14:27:47 +0600

    2.1

---
+++ Algorithm Lab/dijkstra.cpp
@@ -0,0 +1,143 @@
+
+#include<bits/stdc++.h>
+#define INF INT_MAX
+
+using namespace std;
+
+vector<int> topoSorted;
+bool visited[1000] = {false};
+vector<int> parents(1000,-1);
+vector<long long>cost(1000,INF);
+int it= 0;
+
+void dijkstra(vector<pair<int,int>> adj[],int v)
+{
+    visited[v] = true;
+
+    priority_queue<int> q;
+    q.push(v);
+
+    while(!q.empty())
+    {
+        int n = q.top();
+        visited[n] = true;
+        q.pop();
+        for(auto u : adj[n])
+        {
+            it++;
+
+            if(!visited[u.first])
+                if(cost[u.first] > cost[n]+u.second)
+            {
+                q.push(u.first);
+                cost[u.first] = cost[n]+u.second;
+                parents[u.first] = n ;
+            }
+        }
+    }
+}
+
+
+
+void printPath(int s,int d)
+{
+    vector<int>path;
+   /* if(parents[d]==(-1))
+    {
+        cout << "From " << s << " to " << d <<" : ";
+        cout << "Cant reach there." << endl;
+        return;
+    }
+    */
+    int i = d;
+    while(i!=-1)
+    {
+        path.push_back(i);
+        i = parents[i];
+    }
+  //  path.push_back(s);
+
+
+    reverse(path.begin(),path.end());
+
+
+    cout << "From " << s << " to " << d <<" : ";
+    for(auto it : path)
+    {
+        if(it == s)
+        {
+            cout << it;
+        }
+        else
+        {
+            cout << " -> " << it ;
+        }
+
+    }
+
+    cout << " and shorted distance : "<< cost[d]<< endl;
+
+    path.clear();
+}
+
+
+void add_edge(vector<pair<int,int>> adj[],int s,int d,int w)
+{
+    adj[s].push_back(make_pair(d,w));
+    adj[d].push_back(make_pair(s,w));
+}
+
+int main()
+{
+    int v,e;
+    cin >> v>>e;
+    int s,d,w;
+    vector<pair<int,int>> adj[v+1];
+    for(int i =0; i<e; i++)
+    {
+        cin >> s>>d>>w;
+        add_edge(adj,s,d,w);
+    }
+
+
+    int source;
+    cin>> source;
+    cost[source] =0;
+
+    dijkstra(adj,source);
+
+
+    for(int i =1; i<=v; i++)
+    {
+        cout << cost[i] << "/" << parents[i] << " " ;
+    }
+
+    cout << endl;
+
+    for(int i =1; i<=v; i++)
+    {
+        printPath(source,i);
+    }
+
+    cout << endl << it << endl;
+
+
+
+    return 0;
+}
+
+
+/*
+1 2 2
+1 4 4
+4 2 1
+2 8 5
+4 8 3
+3 2 3
+3 5 2
+4 3 3
+5 2 4
+2 7 3
+8 7 4
+8 6 1
+*/
