8967eae960affeaff837e302e81fb952001382e2
Mahmudul Haque Tafhim <54865949+mhtafhim@users.noreply.github.com>
2023-12-13 22:40:50 +0600

    update

--- OS lab/Preemptive Priority Scheduling.cpp
+++ OS lab/Preemptive Priority Scheduling.cpp
@@ -1,108 +1,147 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-struct Process {
-  int id;
-  int burstTime;
-  int arrivalTime;
-  int remainingTime;
-  int priority;
+struct Process
+{
+    int id;
+    int burstTime;
+    int arrivalTime;
+    int priority;
+    int remainingTime;
+    int turnaroundTime = 0;
+    int waitingTime = 0;
+    bool vis = false;
 };
 
-bool compareBurstTime(Process a, Process b) {
-  return a.remainingTime < b.remainingTime;
-}
+struct ComparePriority
+{
+    bool operator()(const Process &a, const Process &b) const
+    {
+        if (a.priority == b.priority)
+        {
+            return a.arrivalTime > b.arrivalTime;
+        }
+        return a.priority > b.priority;
+    }
+};
 
-bool comparePriority(Process a, Process b) {
-  if (a.remainingTime == b.remainingTime) {
-    return a.priority > b.priority;
-  }
-  return a.remainingTime < b.remainingTime;
-}
+void preemptivePriority(Process processes[], int n)
+{
+    std::priority_queue<Process, std::vector<Process>, ComparePriority> readyQueue;
+    //  priority_queue<Process, vector<Process>, comparePriority> readyQueue;
+    int currentTime = 0;
+    int completedProcesses = 0;
 
-void preemptivePriority(Process processes[], int n) {
-  priority_queue<Process, vector<Process>, comparePriority> readyQueue;
-  int currentTime = 0;
-  int completedProcesses = 0;
-
-  // Sort processes by arrival time
-  sort(processes, processes + n, [](const Process& a, const Process& b) {
-    return a.arrivalTime < b.arrivalTime;
-  });
-
-  while (completedProcesses < n) {
-    // Add processes that have arrived to the ready queue
-    for (int i = 0; i < n; i++) {
-      if (processes[i].arrivalTime <= currentTime && !processes[i].remainingTime) {
-        processes[i].remainingTime = processes[i].burstTime;
-        readyQueue.push(processes[i]);
-      }
+    for (int i = 0; i < n; i++)
+    {
+        if (processes[i].arrivalTime <= currentTime && !processes[i].vis)
+        {
+            processes[i].remainingTime = processes[i].burstTime;
+            processes[i].vis = true;
+            readyQueue.push(processes[i]);
+        }
     }
 
-    // If ready queue is not empty, preempt process with shortest remaining time
-    if (!readyQueue.empty()) {
-      Process process = readyQueue.top();
-      readyQueue.pop();
+    while (completedProcesses < n)
+    {
+        // If ready queue is not empty, preempt process with highest priority
+        if (!readyQueue.empty())
+        {
+            Process process = readyQueue.top();
+            readyQueue.pop();
 
-      // Execute process for the minimum of remaining time and its burst time
-      int executedTime = min(process.remainingTime, process.burstTime);
-      process.remainingTime -= executedTime;
-      currentTime += executedTime;
+            // Execute process for the minimum of remaining time and its burst time
+            int executedTime = 1;
+            process.remainingTime -= executedTime;
+            currentTime += executedTime;
+            bool ok = false;
+            //    cout << currentTime << endl;
+            for (int i = 0; i < n; i++)
+            {
+                if (processes[i].arrivalTime <= currentTime && !processes[i].vis)
+                {
+                    ok = true;
+                    processes[i].remainingTime = processes[i].burstTime;
+                    processes[i].vis = true;
+                    readyQueue.push(processes[i]);
+                }
+            }
 
-      // Check for process completion
-      if (process.remainingTime == 0) {
-        process.turnaroundTime = currentTime - process.arrivalTime;
-        process.waitingTime = process.turnaroundTime - process.burstTime;
-        completedProcesses++;
-      } else {
-        readyQueue.push(process);
-      }
-    } else {
-      currentTime++;
+            // Check for process completion
+            if (process.remainingTime == 0)
+            {
+                //  cout << currentTime << endl;
+                processes[process.id - 1].turnaroundTime = currentTime - processes[process.id - 1].arrivalTime;
+                processes[process.id - 1].waitingTime = processes[process.id - 1].turnaroundTime - processes[process.id - 1].burstTime;
+
+                completedProcesses++;
+
+                //  cout << process.id << " " << process.turnaroundTime << " " << process.waitingTime << endl;
+            }
+            else
+            {
+                readyQueue.push(process);
+            }
+        }
+        else
+        {
+            currentTime++;
+        }
     }
-  }
 
-  // Calculate average waiting and turnaround time
-  float avgWaitingTime = 0.0;
-  float avgTurnaroundTime = 0.0;
-  for (int i = 0; i < n; i++) {
-    avgWaitingTime += processes[i].waitingTime;
-    avgTurnaroundTime += processes[i].turnaroundTime;
-  }
-  avgWaitingTime /= n;
-  avgTurnaroundTime /= n;
+    // Calculate average waiting and turnaround time
+    float avgWaitingTime = 0.0;
+    float avgTurnaroundTime = 0.0;
+    for (int i = 0; i < n; i++)
+    {
+        avgWaitingTime += processes[i].waitingTime;
+        avgTurnaroundTime += processes[i].turnaroundTime;
+    }
+    avgWaitingTime /= n;
+    avgTurnaroundTime /= n;
 
-  // Print results
-  cout << "Process\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time" << endl;
-  for (int i = 0; i < n; i++) {
-    cout << processes[i].id << "\t\t" << processes[i].burstTime << "\t\t" << processes[i].arrivalTime << "\t\t" << processes[i].waitingTime << "\t\t" << processes[i].turnaroundTime << endl;
-  }
-  cout << "Average Waiting Time: " << avgWaitingTime << endl;
-  cout << "Average Turnaround Time: " << avgTurnaroundTime << endl;
+    // Print results
+    cout << "\nProcess\tBurst Time\tArrival Time\tPriority\tWaiting Time\tTurnaround Time" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << processes[i].id << "\t\t" << processes[i].burstTime << "\t\t" << processes[i].arrivalTime << "\t\t" << processes[i].priority << "\t\t" << processes[i].waitingTime << "\t\t" << processes[i].turnaroundTime << endl;
+    }
+    cout << "Average Waiting Time: " << avgWaitingTime << endl;
+    cout << "Average Turnaround Time: " << avgTurnaroundTime << endl;
 }
 
-int main() {
-  int n;
+int main()
+{
+    int n;
 
-  cout << "Enter the number of processes: ";
-  cin >> n;
+    //   cout << "Enter the number of processes: ";
+    cin >> n;
 
-  Process processes[n];
+    Process processes[n];
 
-  for (int i = 0; i < n; i++) {
-    cout << "Enter process " << i + 1 << " details:" << endl;
-    cout << "  Burst Time: ";
-    cin >> processes[i].burstTime;
-    cout << "  Arrival Time: ";
-    cin >> processes[i].arrivalTime;
-    cout << "  Priority: ";
-    cin >> processes[i].priority;
-    processes[i].id = i + 1;
-  }
+    for (int i = 0; i < n; i++)
+    {
 
-  preemptivePriority(processes, n);
+        cin >> processes[i].burstTime;
+        processes[i].id = i + 1;
+    }
 
-  return 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> processes[i].arrivalTime;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> processes[i].priority;
+    }
+
+    cout << endl;
+
+    preemptivePriority(processes, n);
+
+    return 0;
 }
