22a3b09e093ce508a40d16089c3bbea2526f29e0
Mahmudul Haque Tafhim <54865949+mhtafhim@users.noreply.github.com>
2023-12-15 23:04:42 +0600

    update

---
+++ new.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<ll>
+#define vpii vector<pii>
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+void solve() {
+    // Your code goes here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
